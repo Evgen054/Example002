19c605a46d28aed3a3bda2b32cedf44804d1a2c1
EVG54 <bankir032@mail.ru>
2022-01-03 16:00:31 +0300

    Example005

---
+++ Exampeles/Example005/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine ("Введите ваше имя");
+string username = Console.ReadLine();
+
+if (username == "Евгений")
+{
+     Console.Write ("Ура! Это же, Евгений!!!");
+}
+
+else
+{
+     Console.Write ("Привет, ");
+     Console.WriteLine(username); 
+}
