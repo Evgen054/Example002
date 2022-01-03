20d2ffa3ad0a78d1dccbc60fe264e5d3d49a9567
EVG54 <bankir032@mail.ru>
2022-01-03 17:56:01 +0300

    Example007 Работает, но не так как хотелось бы 2

--- Exampeles/Example007_magic/Program.cs
+++ Exampeles/Example007_magic/Program.cs
@@ -1,4 +1,5 @@
 ﻿Console.Clear();
+
 // Console.SetCursorPosition();
 // Console.Writeline()
 
@@ -6,20 +7,11 @@ int xa = 20, ya = 1,
     xb = 1, yb = 15,
     xc = 40, yc = 15;
 
-Console.SetCursorPosition(xa, ya);
-Console.WriteLine("+");
-
-Console.SetCursorPosition(xb, yb);
-Console.WriteLine("+");
-
-Console.SetCursorPosition(xc, yc);
-Console.WriteLine("+");
-
 int x = xa, y = xb;
 
 int count = 0;
 
-while (count < 10000)
+while (count < 100)
 {
     int what = new Random().Next(0, 3);
     if (what == 0)
@@ -33,6 +25,7 @@ while (count < 10000)
         x = (x + xb) / 2;
         y = (y + yb) / 2;
     }
+
     if (what == 2)
     {
         x = (x + xc) / 2;
@@ -40,7 +33,7 @@ while (count < 10000)
     }
 
     Console.SetCursorPosition(x, y);
-    Console.WriteLine("+");
+    Console.WriteLine("/");
     count++;
 }
 
