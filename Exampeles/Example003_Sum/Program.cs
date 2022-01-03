a0b95c4cb6e0044b4d0498dc04dbc8a971751c7e
EVG54 <bankir032@mail.ru>
2022-01-03 15:32:21 +0300

    Example003_3

--- Exampeles/Example003_Sum/Program.cs
+++ Exampeles/Example003_Sum/Program.cs
@@ -1,5 +1,12 @@
 ﻿int numberA = new Random().Next(1,10); // Случайное число от 1 до 9
+Console.Write("Число А = ");
+Console.WriteLine(numberA);
+
 int numberB = new Random().Next(1,10);
+Console.Write("Число В = ");
+Console.WriteLine(numberB);
+
+Console.Write("Результат сложения двух чисел = ");
 int result = numberA + numberB;
 
 Console.WriteLine(result);
