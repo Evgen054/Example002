807e37841b2a55a4cd3b91e87d12cdff0ad33ecc
EVG54 <bankir032@mail.ru>
2022-01-05 16:30:05 +0300

    new branch 008

---
+++ Exampeles/Example008/Program.cs
@@ -0,0 +1,10 @@
+﻿int a1 = new Random().Next(1,20);
+int a2 = new Random().Next(1,20);
+int a3 = new Random().Next(1,20);
+int b1 = new Random().Next(1,20);
+int b2 = new Random().Next(1,20);
+int b3 = new Random().Next(1,20);
+int c1 = new Random().Next(1,20);
+int c2 = new Random().Next(1,20);
+int c3 = new Random().Next(1,20);
+
