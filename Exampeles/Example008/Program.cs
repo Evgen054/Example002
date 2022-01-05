58ac76e39d66917c6b9d7f5b57b574f7dbe051f7
EVG54 <bankir032@mail.ru>
2022-01-05 18:18:39 +0300

    new branch 008_3

--- Exampeles/Example008/Program.cs
+++ Exampeles/Example008/Program.cs
@@ -1,4 +1,4 @@
-﻿int Max(int arg1, int arg2, int arg3);
+﻿int Max(int arg1, int arg2, int arg3)
 {
     int result = arg1;
     if (arg2 > result) result = arg2;
@@ -6,19 +6,24 @@
     return result;
 }
 
-int a1 = new Random().Next(1, 20);
-int a2 = new Random().Next(1, 20);
-int a3 = new Random().Next(1, 20);
-int b1 = new Random().Next(1, 20);
-int b2 = new Random().Next(1, 20);
-int b3 = new Random().Next(1, 20);
-int c1 = new Random().Next(1, 20);
-int c2 = new Random().Next(1, 20);
-int c3 = new Random().Next(1, 20);
+int a1 = 1;
+int a2 = 2;
+int a3 = 3;
+int b1 = 4;
+int b2 = 5;
+int b3 = 6;
+int c1 = 7;
+int c2 = 8;
+int c3 = 9;
 
-int max1 = Max(a1, a2, a3);
-int max2 = Max(b1, b2, b3);
-int max3 = Max(c1, c2, c3);
-int max = Max(max1, max2, max3);
+//int max1 = Max(a1, a2, a3);
+//int max2 = Max(b1, b2, b3);
+//int max3 = Max(c1, c2, c3);
+//int max = Max(max1, max2, max3);
+
+ int max = Max(
+   Max(a1, a2, a3),
+   Max(b1, b2, b3),
+   Max(c1, c2, c3)); 
 
 Console.WriteLine(max);
