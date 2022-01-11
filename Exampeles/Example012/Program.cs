721f22ce4c1697b0f4d9c0a9c2949d5f4cdf09c0
EVG54 <bankir032@mail.ru>
2022-01-11 21:40:42 +0300

    Example012_2

--- Exampeles/Example012/Program.cs
+++ Exampeles/Example012/Program.cs
@@ -77,7 +77,7 @@ Console.WriteLine(res);
 вы бы взяли приступом согласие прусского короля.
 Вы так красноречивы. Вы дадите мне чаю?  */
 
-string text = "– Я думаю, – сказал князь, улыбаясь, – что,"
+/* string text = "– Я думаю, – сказал князь, улыбаясь, – что,"
             + "ежели бы вас послали вместо нашего милого Винценгероде,"
             + "вы бы взяли приступом согласие прусского короля."
             + "Вы так красноречивы. Вы дадите мне чаю?";
@@ -95,8 +95,72 @@ string Replace(string text, char OldValue, char NewValue)
 
     return result;
 }
-string newtext = Replace(text,' ', '|');
+string newtext = Replace(text, ' ', '|');
 Console.WriteLine(newtext);
+Console.WriteLine();
+
+newtext = Replace(newtext, 'к', 'K');
+Console.WriteLine(newtext); */
 
 
+int[] arr = { 5, 8, 7, 4, 3, 6, 9, 1, 5, 4 };
 
+/* void PrintArray(int[], Array) */
+
+void PrintArray(int[] array)
+{
+
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int minPosition = i;
+        for (int j = i+1; j < array.Length; j++)
+        {
+            if (array[j]< array [minPosition])
+            {
+                minPosition = j;
+            }
+        }
+        int temporary = array[i];
+        array [i] = array [minPosition];
+        array [minPosition] = temporary;
+    }
+
+}
+
+void SelectionSortMax(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int maxPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition])
+            {
+                maxPosition = j;
+            }
+        }
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
+SelectionSortMax(arr);
+
+PrintArray(arr);
