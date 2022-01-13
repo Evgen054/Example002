2332588c50a1cd8d43704726d14a27262ab4f11d
EVG54 <bankir032@mail.ru>
2022-01-13 21:04:15 +0300

    lesson 23

---
+++ Exampeles/урок1/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = new Random().Next(1, 10);
+
+int i = 1;
+while (i < n)
+{
+    int kvadratchisla = i * i;
+   
+    Console.WriteLine("Квадрат числа " + i + " = " + squarenumbers);
+    i++;
+
+}
