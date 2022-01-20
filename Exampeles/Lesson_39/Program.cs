c22e8c797ff96a115ef8fb571139c6afe8827164
EVG54 <bankir032@mail.ru>
2022-01-20 21:53:47 +0300

    lesson 38

---
+++ Exampeles/Lesson_39/Program.cs
@@ -0,0 +1,27 @@
+﻿double[] Array = { 4, 6, 18, 10, 12, 14, 16, };
+
+double MaxNumber(double[] Array)
+{
+  double max = Array[0];
+  for (int i = 0; i < Array.Length; i++)
+  {
+    if (Array[i] > max) max = Array[i];
+  }
+  return max;
+}
+
+double MinNumber(double[] Array)
+{
+  double min = Array[0];
+  for (int i = 0; i < Array.Length; i++)
+  {
+    if (Array[i] < min) min = Array[i];
+  }
+  return min;
+}
+
+double a = MaxNumber(Array); 
+System.Console.WriteLine(a);
+double b = MinNumber(Array); 
+System.Console.WriteLine(b);
+System.Console.WriteLine(a-b);
