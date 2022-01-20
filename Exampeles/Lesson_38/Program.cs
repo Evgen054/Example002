aa53963b5271cfe22d5e49160f3346b0e8aa7e4b
EVG54 <bankir032@mail.ru>
2022-01-20 20:34:48 +0300

    lesson 37

---
+++ Exampeles/Lesson_38/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] somearray = new int[10];
+
+void FillArray(int[] newarray)
+{
+    for (int i = 0; i < newarray.Length; i++)
+    {
+        newarray[i] = new Random().Next(1, 1000);
+        Console.Write($"{newarray[i]} ");
+    }
+}
+void CheckMassNumber(int[] checkedarray)
+{
+    int NumberCounter = 0;
+    for (int j = 0; j < checkedarray.Length; j++)
+    {
+        if (checkedarray[j] < 100 && checkedarray[j] > 9)
+        {
+            NumberCounter = NumberCounter + 1;
+        }
+    }
+Console.WriteLine("");
+Console.WriteLine($"В массиве {NumberCounter} чисел из дипазона [10,99]");
+}
+FillArray(somearray);
+CheckMassNumber(somearray);
+
